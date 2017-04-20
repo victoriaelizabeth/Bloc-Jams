7a33bd70154f2460e9c9a2a8293b389dabf91195
Victoria Stubbolo <victoriastubbolo@Victorias-MacBook-Pro.local>
2017-04-20 16:29:34 -0700

    checkpoint 9: completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,6 +1,6 @@
-var animatePoints = function() {
- 
-     var points = document.getElementsByClassName('point');
+var pointsArray = document.getElementsByClassName('point');
+
+var animatePoints = function(points) {
  
     var revealPoint = function(index) {
          points[index].style.opacity = 1;
@@ -13,6 +13,21 @@ var animatePoints = function() {
     for (var i=0; i < points.length; i++) {
      revealPoint(i);
 }
- };
+    };
 
-animatePoints();
+window.onload = function(){
+    
+    if (window.innerHeight > 950) {
+        animatePoints(pointsArray);
+    }
+    
+    var sellingPoints = document.getElementsByClassName('selling-points')[0];
+    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+    
+    window.addEventListener("scroll", function(event) {
+        if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+            animatePoints(pointsArray);
+        }
+    });
+}
+
