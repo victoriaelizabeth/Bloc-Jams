e7cf732b91330c6063937042b74f46df4dc6ef67
Victoria Stubbolo <victoriastubbolo@Victorias-MacBook-Pro.local>
2017-04-25 14:18:17 -0700

    assignment 13 completed

--- scripts/album.js
+++ scripts/album.js
@@ -68,6 +68,12 @@ var setCurrentAlbum = function(album) {
 var findParentByClassName = function(element, targetClass) {
     if (element) {
         var currentParent = element.parentElement;
+    if (currentParent === null) {
+        console.log("No parent found.");
+    }
+    if (currentParent !== targetClass) {
+        console.log("No parent found with that class name."); 
+    }
         while (currentParent.className !== targetClass && currentParent.className !== null) {
             currentParent = currentParent.parentElement;
         }
